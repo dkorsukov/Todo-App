f29f245a1889ba45980f4d7c6fe155be946f192f
Zilfaron <42319249+Zilfaron@users.noreply.github.com>
2019-06-22 16:11:48 +0500

    Create files of navigation components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template lang="pug">
-	
+	v-app
+		v-container(fluid, pa-0)
+			v-layout(row)
+				navigation
 </template>
 
 <script>
+	import navigation from "@components/navigation.vue";
+
 	export default {
-		
+		data() {
+			return {
+				
+			};
+		},
+
+		components: {
+			navigation
+		}
 	}
 </script>
 
---
+++ src/components/navigation.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+	
+</template>
+
+<script>
+	import authSection from "@components/auth.vue";
+
+	export default {
+		components: {
+			authSection
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
