d2c59f79d486e0f8bdef97044dfcda49b20b360f
dkorsukov <42319249+dkorsukov@users.noreply.github.com>
2019-06-27 20:27:09 +0500

    Create component with all popups

---
+++ src/components/windows.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+	create-account-popup
+</template>
+
+<script>
+	import createAccount from "./windows/create_account.vue";
+
+	export default {
+		components: {
+			createAccountPopup
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
