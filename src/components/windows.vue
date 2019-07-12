322c69f03d0f1f0a66ad0044e781ffe097a581c6
dkorsukov <42319249+dkorsukov@users.noreply.github.com>
2019-07-12 23:09:13 +0500

    Create component for popups

--- src/components/windows.vue
+++ src/components/windows.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-	create-account-popup
+	create-account
 </template>
 
 <script>
@@ -7,7 +7,7 @@
 
 	export default {
 		components: {
-			createAccountPopup
+			createAccount
 		}
 	}
 </script>
