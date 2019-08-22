40477dbfe16ebc5e3cce087d7110effc560d584a
dkorsukov <42319249+dkorsukov@users.noreply.github.com>
2019-08-22 10:55:06 +0500

    Create a toolbar for mobile version

---
+++ src/components/toolbar.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+	v-toolbar(color="primary")
+		v-btn(icon, flat, large, color="white",
+					@click="showNavigation")
+			v-icon(medium, color="white") menu
+		v-toolbar-title.white--text Todo-App
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		},
+
+		methods: {
+			showNavigation() {
+				this.$store.commit("setNavigationVisibility", true);
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
