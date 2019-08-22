d3c14472f176c2a7ee2579a938c45d1a2dcefd19
dkorsukov <42319249+dkorsukov@users.noreply.github.com>
2019-08-22 10:56:51 +0500

    Create component of todo list

---
+++ src/components/main/todo_list.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+	v-layout.mt-2(row, wrap)
+		todo-item(v-for="(todo, index) in todos",
+							:key="index",
+							:priority="todo.priority",
+							:title="todo.title") {{ todo.description }}
+</template>
+
+<script>
+	import todoItem from "./todo_list/todo.vue";
+
+	export default {
+		data() {
+			return {
+
+			};
+		},
+
+		computed: {
+			todos() {
+				return this.$store.getters.sortedTodos;
+			}
+		},
+
+		components: {
+			todoItem
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
