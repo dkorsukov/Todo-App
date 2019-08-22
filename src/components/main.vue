3f92163bf113966a69c95f97a95509f21529f819
dkorsukov <42319249+dkorsukov@users.noreply.github.com>
2019-08-22 10:54:15 +0500

    Create a primary component of main section

---
+++ src/components/main.vue
@@ -0,0 +1,68 @@
+<template lang="pug">
+	v-content
+		v-container.main-content__container(fluid, pa-4)
+			tooltip
+			tools
+			todo-list
+			v-speed-dial.create-todo-btn(fixed, bottom, right, 
+																	v-if="isCurrentFolderLoaded")
+				template(#activator)
+					v-btn(fab, color="primary", @click='$store.commit("setCreatingTodoPopup", true)')
+						v-icon(:size="30") add
+</template>
+
+<script>
+	import { mapState } from "vuex";
+	import tooltip from "./main/tooltip.vue";
+	import tools from "./main/tools.vue";
+	import todoList from "./main/todo_list.vue"; 
+
+	export default {
+		data() {
+			return {
+
+			};
+		},
+
+		computed: {
+			...mapState({
+				isCurrentFolderLoaded: (state) => state.folders.isCurrentFolderLoaded
+			})
+		},
+
+		components: {
+			tooltip, tools, todoList
+		}
+	}
+</script>
+
+<style lang="scss">
+	.main {
+		width: 100%;
+
+		&-content__container {
+			position: relative;
+			min-height: 100%;
+		}
+	}
+
+	.order-select {
+		margin-top: -15px;
+		margin-left: 10px;
+		max-width: 85px;
+	}
+
+	.normal-size {
+		font-size: 16px;
+	}
+
+	.create-todo-btn {
+		right: 32px;
+		bottom: 32px;
+
+		@media screen and (max-width: 621px) {
+			right: 16px;
+			bottom: 16px;
+		}
+	}
+</style>
