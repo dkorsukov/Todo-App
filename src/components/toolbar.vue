2f8c81013ce4b9392c51569727fd55739a5ac962
dkorsukov <42319249+dkorsukov@users.noreply.github.com>
2019-09-07 20:05:15 +0500

    Add padding for toolbar content

--- src/components/toolbar.vue
+++ src/components/toolbar.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-	v-toolbar(color="primary")
+	v-toolbar.pa-1(color="primary")
 		v-btn(icon, flat, large, color="white",
 					@click="showNavigation")
 			v-icon(medium, color="white") menu
