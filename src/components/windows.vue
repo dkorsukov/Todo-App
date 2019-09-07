a18a1a28bb969d771bfef8e3a68073e8caa77540
dkorsukov <42319249+dkorsukov@users.noreply.github.com>
2019-09-07 20:05:30 +0500

    Create a reset account popup component

--- src/components/windows.vue
+++ src/components/windows.vue
@@ -1,18 +1,21 @@
 <template lang="pug">
 	.windows
 		create-account
+		reset-password
 		create-folder
 		create-todo
 </template>
 
 <script>
 	import createAccount from "./windows/create_account.vue";
+	import resetPassword from "./windows/reset_password.vue";
 	import createFolder from "./windows/create_folder.vue";
 	import createTodo from "./windows/create_todo.vue";
 
 	export default {
 		components: {
 			createAccount,
+			resetPassword,
 			createFolder,
 			createTodo
 		}
